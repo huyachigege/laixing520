4e174c36a0b69900b1744928ff577818d864105c
yinchi <yinchi910517@live.com>
2018-11-16 09:33:59 +0800

    1

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/IBrandService.java
@@ -0,0 +1,9 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+public interface IBrandService {
+    public List<TbBrand> findAll();
+}
